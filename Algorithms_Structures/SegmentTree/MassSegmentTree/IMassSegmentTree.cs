48299e80f4a1fcabf7307d9e247a6f052bc2bc72
Evgeny.Terekhin <Evgeny.Terekhin@jetbrains.com>
2024-07-17 18:55:30 +0200

    Removed redundant members and "public"s

--- Algorithms_Structures/SegmentTree/MassSegmentTree/IMassSegmentTree.cs
+++ Algorithms_Structures/SegmentTree/MassSegmentTree/IMassSegmentTree.cs
@@ -2,14 +2,11 @@ namespace Algorithms_Structures.SegmentTree.MassSegmentTree;
 
 public interface IMassMergeableValue<TSelf>
 {
-    public bool HasNothingToPropagate();
-    public static abstract TSelf CreateDefaultValue { get; }
-    public static abstract TSelf NeutralResultValue { get; }
-    public static abstract TSelf MergeResults(TSelf left, TSelf right);
-    public static abstract TSelf ApplyValue(TSelf a, TSelf value);
-
-    public long ValueAppliedToSegment { get; }
-    public long ResultValue { get; }
+    bool HasNothingToPropagate();
+    static abstract TSelf CreateDefaultValue { get; }
+    static abstract TSelf NeutralResultValue { get; }
+    static abstract TSelf MergeResults(TSelf left, TSelf right);
+    static abstract TSelf ApplyValue(TSelf a, TSelf value);
 }
 
 public interface IMassSegmentTree<T> where T : IMassMergeableValue<T>
